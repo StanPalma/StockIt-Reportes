4e239c5c5c37a6eedd66880b32c7d1e9e1aa1052
StanSixx <82747581+StanSixx@users.noreply.github.com>
2021-11-01 16:12:33 -0600

    Initial commit

---
+++ Electiva4/Controllers/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Electiva4.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Productos()
+        {
+            return View();
+        }
+
+        public ActionResult Clientes()
+        {
+            return View();
+        }
+
+        public ActionResult Reservas()
+        {
+            return View();
+        }
+
+        public ActionResult Ventas()
+        {
+            return View();
+        }
+    }
+}
