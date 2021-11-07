8af1f9d7a49c6df47d6133b8b46bfe8117876516
StanSixx <82747581+StanSixx@users.noreply.github.com>
2021-11-07 10:05:19 -0600

    Filtro para reportes

--- Electiva4/Controllers/MenuController.cs
+++ Electiva4/Controllers/MenuController.cs
@@ -16,6 +16,19 @@ namespace Electiva4.Controllers
 
         public ActionResult Productos()
         {
+            List<SelectListItem> categorias = new List<SelectListItem>();
+            categorias.Add(new SelectListItem() { Text = "categorias 1", Value = "1" });
+            categorias.Add(new SelectListItem() { Text = "categorias 2", Value = "2" });
+            categorias.Add(new SelectListItem() { Text = "categorias 3", Value = "3" });
+
+            List<SelectListItem> estado = new List<SelectListItem>();
+            estado.Add(new SelectListItem() { Text = "estado 1", Value = "1" });
+            estado.Add(new SelectListItem() { Text = "estado 2", Value = "2" });
+            estado.Add(new SelectListItem() { Text = "estado 3", Value = "3" });
+
+            // Enviar lista a ViewBag para recibir en la Vista
+            ViewBag.categorias = categorias;
+            ViewBag.estado = estado;
             return View();
         }
 
@@ -31,6 +44,14 @@ namespace Electiva4.Controllers
 
         public ActionResult Ventas()
         {
+            List<SelectListItem> clientes = new List<SelectListItem>();
+            clientes.Add(new SelectListItem() { Text = "Cliente 1", Value = "1"});
+            clientes.Add(new SelectListItem() { Text = "Cliente 2", Value = "2" });
+            clientes.Add(new SelectListItem() { Text = "Cliente 3", Value = "3" });
+
+            // Enviar lista a ViewBag para recibir en la Vista
+            ViewBag.clientes = clientes;
+
             return View();
         }
     }
---
+++ Electiva4/Models/ViewModels/TablaVentas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Electiva4.Models.ViewModels
+{
+    public class TablaVentas
+    {
+        public int id { get; set; }
+    }
+}
