57ee710dd61b4abbb00239d97d5599b4d1963b1e
AmiltonR <32588294+AmiltonR@users.noreply.github.com>
2021-11-11 20:30:24 -0600

    Add Login Form

---
+++ Electiva4/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Electiva4.Models;
+using System.Web.Mvc;
+namespace Electiva4.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Verify(Login login )
+        {
+            return View();
+        }
+    }
+}
